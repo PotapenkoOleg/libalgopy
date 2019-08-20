595fcdd181d97b99ce75fc425ad9437badee687a
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-08-20 16:59:46 -0400

    Starter hash tables

---
+++ libalgopy/hash_tables/hash_table_linear_probing.py
@@ -0,0 +1,25 @@
+from common.interfaces.hash_table import HashTableBase
+
+
+class HashTableLinearProbing(HashTableBase):
+    def add(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
+
+    def remove(self, key):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def clear(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ libalgopy/tests/hash_tables/test_hash_table_linear_probing.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestsHashTableLinearProbing(TestCase):
+    def test_add(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_is_empty(self):
+        self.fail()
+
+    def test_get_size(self):
+        self.fail()
+
+    def test_clear(self):
+        self.fail()
