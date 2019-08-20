493d80c4a0d9e0a6a1bde6f922d180a83c730e1d
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-08-20 17:07:58 -0400

    Queues starter

---
+++ libalgopy/queues/array_queue.py
@@ -0,0 +1,22 @@
+class ArrayQueue():
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def get_size(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ libalgopy/queues/linked_list_queue.py
@@ -0,0 +1,22 @@
+class LinkedListQueue():
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def get_size(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
