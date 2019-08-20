c6328a2d069121d5e023447b230188d8a7abfab2
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-08-20 16:32:46 -0400

    Lists starter

---
+++ libalgopy/lists/single_linked_list.py
@@ -0,0 +1,31 @@
+from common.interfaces.list import ListBase
+
+
+class SingleLinkedList(ListBase):
+    def add_at_position(self, key, value, position):
+        pass
+
+    def add_before(self, key, value, before):
+        pass
+
+    def remove_at_position(self, position):
+        pass
+
+    def remove(self, key):
+        pass
+
+    def get(self, key):
+        pass
+
+    def clear(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def get_size(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
