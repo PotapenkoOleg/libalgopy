c206818d5248b7901a97c9aa6fa9d34968c261f1
PotapenkoOleg <PotapenkoOleg@gmail.com>
2019-08-20 17:03:09 -0400

    Lists tests

---
+++ libalgopy/tests/lists/test_single_linked_list.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestsSingleLinkedList(TestCase):
+    def test_add_at_position(self):
+        self.fail()
+
+    def test_add_before(self):
+        self.fail()
+
+    def test_remove_at_position(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
+
+    def test_clear(self):
+        self.fail()
+
+    def test_is_empty(self):
+        self.fail()
+
+    def test_get_size(self):
+        self.fail()
